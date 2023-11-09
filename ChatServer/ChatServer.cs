068b4c6bdbccdd254e670fa0912293ce2e2aa956
Crawford Hardie-Grainger <chardiegrainger@gmail.com>
2023-11-09 15:13:35 +1300

    first commit

---
+++ ChatServer/ChatServer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+namespace ChatServer
+{
+	public class ChatServer
+	{
+
+		int PORT = 1998;
+		//String HOST = "localhost";
+
+		public ChatServer()
+		{
+			while (true) {
+				TcpListener listener = new TcpListener(IPAddress.Any, PORT);
+			}
+		}
+	}
+}
+
---
+++ ChatServer/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+namespace ChatServer
+{
+	public class Client
+	{
+		TcpClient client;
+		public Client(TcpClient client)
+		{
+			this.client = client;
+		}
+	}
+}
+
